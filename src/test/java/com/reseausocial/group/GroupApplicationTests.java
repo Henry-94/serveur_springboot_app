681d744fa9581a1d2554d52f7edd2f407584e9bd
Henri Parfait RANOMENJANAHARY <parfaitranomenjanahary@gmail.com>
2026-08-19 12:48:08 +0300

    Configure Render deployment

--- src/test/java/com/reseausocial/group/GroupApplicationTests.java
+++ src/test/java/com/reseausocial/group/GroupApplicationTests.java
@@ -2,8 +2,10 @@ package com.reseausocial.group;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GroupApplicationTests {
 
 	@Test
